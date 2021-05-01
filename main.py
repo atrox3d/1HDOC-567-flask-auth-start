b5b41e868bcac3aa6e314dce4831c66e30fe7ee5
atrox3d <atrox3d@gmail.com>
2021-05-01 08:15:01 +0200

    add logger

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import logging
+
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 import util.network
+import util.logging
+
+util.logging.get_root_logger()
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -12,26 +18,35 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
-# db.create_all()
+    # plainpassword = db.Column(db.String(100))
+
+
+# Line below only required once, when creating DB.
+logger.warning("DELETING tables")
+db.drop_all()
+logger.info("creating tables")
+db.create_all()
 
 
 @app.route('/')
+@util.logging.log_decorator
 def home():
     return render_template("index.html")
 
 
 @app.route('/register', methods=["GET", "POST"])
+@util.logging.log_decorator
 def register():
-    print(f"{request.method=}")
+    logger.info(f"{request.method=}")
     if request.method == "POST":
-        print(f"{request.form=}")
+        logger.info(f"{request.form=}")
         hashed_password = generate_password_hash(
             request.form.get("password"),
             method="pbkdf2:sha256",
@@ -42,10 +57,10 @@ def register():
             email=request.form.get("email"),
             password=hashed_password,
         )
-        print("INFO| add user:")
-        print(f"{user.name=}")
-        print(f"{user.email=}")
-        print(f"{user.password=}")
+        logger.info("INFO| add user:")
+        logger.info(f"{user.name=}")
+        logger.info(f"{user.email=}")
+        logger.info(f"{user.password=}")
         db.session.add(user)
         db.session.commit()
         return redirect(url_for("secrets", name=user.name))
@@ -53,15 +68,23 @@ def register():
 
 
 @app.route('/login')
+@util.logging.log_decorator
 def login():
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@util.logging.log_decorator
 def secrets():
     name = request.args.get("name")
     print(f"{name=}")
     return render_template("secrets.html", name=name)
+
+
+@app.route('/download/<path:filename>')
+@util.logging.log_decorator
+def download(filename):
+    return send_from_directory('static', filename=f"files/{filename}")
 
 
 @app.route('/logout')
@@ -69,9 +92,8 @@ def logout():
     pass
 
 
-@app.route('/download/<path:filename>')
-def download(filename):
-    return send_from_directory('static', filename=f"files/{filename}")
-
 if __name__ == "__main__":
-    app.run(debug=True, host=util.network.get_ipaddress())
+    app.run(
+        debug=True,
+        host=util.network.get_ipaddress()
+    )
